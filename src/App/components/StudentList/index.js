6ad1331ae9ba5bd17e6b326fc2b6ba52b8eeb718
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-07 16:36:22 +0800

    [ruiling] feat: fix css

--- src/App/components/StudentList/index.js
+++ src/App/components/StudentList/index.js
@@ -22,11 +22,14 @@ class StudentList extends React.Component {
   }
 
   render() {
-    return <div>
-      <h1>学员列表</h1>
-      <div className="studentList">{this.state.studentList.map(student => {
+    return <div className="studentDiv">
+      <h1 style={{margin: 10}}>学员列表</h1>
+      <div className="studentList">
+        {this.state.studentList.map(student => {
         return (<div key={student.id} className="student"><p>{student.id}.{student.name}</p></div>)
-      })}</div>
+      })}
+
+      </div>
     </div>;
   }
 }
